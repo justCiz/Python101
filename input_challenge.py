f971ea30d91779e12408c4f34d539087bec11a92
Francis Rojas <ftrojas027@gmail.com>
2018-08-07 20:00:12 +0800

    Create input challenge

---
+++ input_challenge.py
@@ -0,0 +1,18 @@
+""" The Challenge
+Author:
+Description: Aling Nena’s Sari-sari store wants a robot that will ask the
+customer their total bill and payment amount and tell them their change
+(for now, we can allow negative change).
+"""
+
+"""
+totalBill = input("Enter Total Bill: ")
+paymentAmount = input("Enter Payment Amount: ")
+change = totalBill - paymentAmount
+print("Total bill: {0}\n Payment Amount: {1}\n Your change is Php{2}".format(totalBill, paymentAmount, change))
+"""
+
+totalBill = input("How much is your total bill? ")
+paymentAmount = input("How much is your payment? ")
+change = float(totalBill) - float(paymentAmount)
+print("Hi! Your change Php{0}".format(change))
